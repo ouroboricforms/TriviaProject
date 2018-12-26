94fc775fc7914c39cb842d77805c928aee0985ee
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2018-12-26 12:16:34 -0500

    logo added to header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Image from 'react-image-resizer';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header/>
       </div>
     );
   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <img src="TQlogo.png" alt="Logo"
+              height={100}
+              width={400}/>
+         </div>
+      );
+   }
 }
 
 export default App;
